868612145a3511f5d501a44d530a37b121a619b8
tslight <tslight@pm.com>
2022-01-11 10:00:45 +0000

    fix broken test due to archived entity

--- tests/test_oesdk.py
+++ tests/test_oesdk.py
@@ -12,23 +12,23 @@ class TestOesdk(unittest.TestCase):
     def setUpClass(cls):
         cls.username = os.environ["OE_USERNAME"]
         cls.password = os.environ["OE_PASSWORD"]
-        cls.entity_code = "l4662"
+        cls.entity_code = "L2510"
 
     def test_entity_api(self):
         entity_api = EntityApi(self.username, self.password)
         entity_dict = entity_api.entityDetailsAsDict(self.entity_code)
-        assert entity_dict["code"] == "l4662"
+        assert entity_dict["code"].upper() == self.entity_code.upper()
 
         entity_hierarchy_dict = entity_api.explainHierarchy(self.entity_code)
-        assert entity_hierarchy_dict["EntityName"] == "SDK dummy load"
+        assert entity_hierarchy_dict["EntityName"] == "Battery"
 
     def test_demand_api(self):
         demand_api = DemandApi(self.username, self.password)
-        target_date = datetime.datetime(2019, 12, 1).strftime("%Y-%m-%d")
+        target_date = datetime.datetime(2021, 12, 1).strftime("%Y-%m-%d")
         active_profile_df = demand_api.getActiveProfile(self.entity_code, target_date)
         assert len(active_profile_df) >= 0 and len(active_profile_df) <= 48
 
-        iso_weekday_id = datetime.datetime(2019, 12, 1).isoweekday()
+        iso_weekday_id = datetime.datetime(2021, 12, 1).isoweekday()
         default_profile_df = demand_api.getDefaultProfile(
             self.entity_code, iso_weekday_id
         )
@@ -39,12 +39,12 @@ class TestOesdk(unittest.TestCase):
         power_variable = "active-power"
 
         raw_readings_df = historical_api.getRawReadings(
-            "2019-12-01 10:00:00",
-            Timestamp("2019-12-01 12:00:00"),
+            "2021-12-01 10:00:00",
+            Timestamp("2021-12-01 10:01:00"),
             variable=power_variable,
             entity_code=self.entity_code,
         )
-        assert len(raw_readings_df) == 3
+        assert len(raw_readings_df) == 32
 
         resampled_readings_df = historical_api.getResampledReadings(
             "2019-12-01 15",
